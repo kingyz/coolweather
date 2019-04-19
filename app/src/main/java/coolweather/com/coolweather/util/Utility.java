7a7ee7f4c58cd262b1abf7100a7b230dbefd2c29
kingyz <761294412@qq.com>
2019-04-19 13:25:46 +0800

    测试2

--- app/src/main/java/coolweather/com/coolweather/util/Utility.java
+++ app/src/main/java/coolweather/com/coolweather/util/Utility.java
@@ -52,4 +52,24 @@ public class Utility {
         return false;
     }
 
+    public static boolean handleCountyResponse(String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length(); i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
